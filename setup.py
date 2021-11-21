962f579bfbff3efc1b89280d79eb83c0f6328ff6
DDie <danielcuznetov04@gmail.com>
2021-11-21 11:40:44 +0200

    Rename

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
-version = '0.0.3.3.1'
+version = '0.0.3.3.2'
 
 setup(
-    name='django-rutine',
+    name='django-routine',
     version=version,
     description='A Django app for managing routines.',
     author='Daniel Cuznetov',
     author_email='danielcuznetov04@gmail.com',
-    packages=['django_rutine'],
+    packages=['django_routine'],
     entry_points={
         "console_scripts": [
-            "startapp = django_rutine.start:start",
+            "startapp = django_routine.start:start",
 
         ]
     },
