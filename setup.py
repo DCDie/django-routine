c24ef9160f06d905183f5602444868f5f7c4d10e
Daniel Cuznetov <daniel.cuznetov@ebs-integrator.com>
2022-06-22 12:20:27 +0300

    Redact build

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 
-version = '0.0.5.6'
+version = '0.1'
 
-setup(
-    name='django-routine',
-    version=version,
-    description='A Django app for managing routines.',
-    author='Daniel Cuznetov',
-    author_email='danielcuznetov04@gmail.com',
-    packages=['django_routine'],
-    entry_points={
-        "console_scripts": [
-            "startproject = django_routine.start:start",
+if __name__ == '__main__':
+    setup(
+        name='django-routine',
+        version=version,
+        description='A Django app for managing routines.',
+        author='Daniel Cuznetov',
+        author_email='danielcuznetov04@gmail.com',
+        packages=['django_routine'],
+        entry_points={
+            "console_scripts": [
+                "startproject = django_routine.start:start",
 
-        ]
-    },
-    install_requires=['djangorestframework', 'django', 'django-rest-swagger', 'drf_yasg',
-                      'djangorestframework-simplejwt',
-                      ],
-)
+            ]
+        },
+        install_requires=['djangorestframework', 'django', 'django-rest-swagger', 'drf_yasg',
+                          'djangorestframework-simplejwt',
+                          ],
+    )
